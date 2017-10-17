9b8e749a976290da1e8fa0d96db0884d64bb370d
huxiaowei008 <huxiaowei008@126.com>
2017-10-17 15:51:54 +0800

    10月17日 沉浸式状态栏

--- frame/src/main/java/com/hxw/frame/utils/UIUtils.kt
+++ frame/src/main/java/com/hxw/frame/utils/UIUtils.kt
@@ -1,7 +1,11 @@
 package com.hxw.frame.utils
 
+import android.app.Activity
+import android.graphics.Color
+import android.os.Build
 import android.support.design.widget.Snackbar
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import com.hxw.frame.base.delegate.AppDelegate
 import com.hxw.frame.integration.AppManager
@@ -26,5 +30,19 @@ object UIUtils {
 
     fun toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
         Toast.makeText(AppDelegate.instance, message, duration).show()
+    }
+
+    /**
+     * 沉浸式状态栏
+     */
+    fun noStatusBar(activity: Activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.window.statusBarColor = Color.TRANSPARENT
+            activity.window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            activity.window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        }
     }
 }
