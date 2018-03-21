76bda76c391dd81ce6dd2168d1ceef154c51061c
huxiaowei <huxiaowei008@126.com>
2018-03-21 13:35:29 +0800

    3/21 简单使用扩展尝试3

--- app/src/main/java/com/modoutech/kotlin/mvp/MainActivity.kt
+++ app/src/main/java/com/modoutech/kotlin/mvp/MainActivity.kt
@@ -1,21 +1,23 @@
 package com.modoutech.kotlin.mvp
 
-import android.graphics.Color
 import android.os.Bundle
-import com.hxw.frame.base.BaseActivity
+import android.support.v7.app.AppCompatActivity
 import com.modoutech.kotlin.R
-import kotlinx.android.synthetic.main.activity_main.*
+
 /**
  * @author hxw
  * @date 2017/9/19
  */
-class MainActivity : BaseActivity() {
-    override fun getLayoutId(): Int = R.layout.activity_main
-
-
-    override fun init(savedInstanceState: Bundle?) {
-        txt_hello.text = "是对方空间"
-        txt_hello.setTextColor(Color.BLUE)
+class MainActivity : AppCompatActivity() {
+    //    override fun getLayoutId(): Int = R.layout.activity_main
+//
+//
+//    override fun init(savedInstanceState: Bundle?) {
+//        txt_hello.text = "是对方空间"
+//        txt_hello.setTextColor(Color.BLUE)
+//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
-
 }
