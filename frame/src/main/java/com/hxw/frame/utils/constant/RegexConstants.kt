7053eebe00a3a36b61ee606c7fbd9299f2997a80
huxiaowei <huxiaowei008@126.com>
2018-05-05 14:51:02 +0800

    gradle升级

--- frame/src/main/java/com/hxw/frame/utils/constant/RegexConstants.kt
+++ frame/src/main/java/com/hxw/frame/utils/constant/RegexConstants.kt
@@ -9,7 +9,7 @@ object RegexConstants {
     /**
      * 正则：手机号（简单）
      */
-    @JvmField val REGEX_MOBILE_SIMPLE = "^[1]\\d{10}$"
+    const val REGEX_MOBILE_SIMPLE = "^[1]\\d{10}$"
     /**
      * 正则：手机号（精确）
      * <p>移动：134(0-8)、135、136、137、138、139、147、150、151、152、157、158、159、178、182、183、184、187、188、198</p>
@@ -18,48 +18,52 @@ object RegexConstants {
      * <p>全球星：1349</p>
      * <p>虚拟运营商：170</p>
      */
-    @JvmField val REGEX_MOBILE_EXACT = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(16[6])|(17[0,1,3,5-8])|(18[0-9])|(19[8,9]))\\d{8}$"
+    const val REGEX_MOBILE_EXACT = "^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(16[6])|(17[0,1,3,5-8])|(18[0-9])|(19[8,9]))\\d{8}$"
     /**
      * 正则：电话号码
      */
-    @JvmField val REGEX_TEL = "^0\\d{2,3}[- ]?\\d{7,8}"
+    const val REGEX_TEL = "^0\\d{2,3}[- ]?\\d{7,8}"
     /**
      * 正则：身份证号码15位
      */
-    @JvmField val REGEX_ID_CARD15 = "^[1-9]\\d{7}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}$"
+    const val REGEX_ID_CARD15 = "^[1-9]\\d{7}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}$"
     /**
      * 正则：身份证号码18位
      */
-    @JvmField val REGEX_ID_CARD18 = "^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}([0-9Xx])$"
+    const val REGEX_ID_CARD18 = "^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}([0-9Xx])$"
     /**
      * 正则：邮箱
      */
-    @JvmField val REGEX_EMAIL = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
+    const val REGEX_EMAIL = "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$"
     /**
      * 正则：URL
      */
-    @JvmField val REGEX_URL = "[a-zA-z]+://[^\\s]*"
+    const val REGEX_URL = "[a-zA-z]+://[^\\s]*"
     /**
      * 正则：汉字
      */
-    @JvmField val REGEX_ZH = "^[\\u4e00-\\u9fa5]+$"
+    const val REGEX_ZH = "^[\\u4e00-\\u9fa5]+$"
     /**
      * 正则：用户名，取值范围为a-z,A-Z,0-9,"_",汉字，不能以"_"结尾,用户名必须是6-20位
      */
-    @JvmField val REGEX_USERNAME = "^[\\w\\u4e00-\\u9fa5]{6,20}(?<!_)$"
+    const val REGEX_USERNAME = "^[\\w\\u4e00-\\u9fa5]{6,20}(?<!_)$"
     /**
      * 正则：yyyy-MM-dd格式的日期校验，已考虑平闰年
      */
-    @JvmField val REGEX_DATE = "^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)$"
+    const val REGEX_DATE = "^(?:(?!0000)[0-9]{4}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[0-9]{2}(?:0[48]|[2468][048]|[13579][26])|(?:0[48]|[2468][048]|[13579][26])00)-02-29)$"
     /**
      * 正则：IP地址
      */
-    @JvmField val REGEX_IP = "((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)"
+    const val REGEX_IP = "((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)"
     /**
      * 正则：车牌
      */
-    @JvmField val REGEX_LICENSE = "^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$"
+    const val REGEX_LICENSE = "^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$"
 
+    /**
+     * 正则：车牌
+     */
+    const val REGEX_NEWENERGY_LICENSE = "^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}([DF][A-HJ-NP-Z0-9][0-9]{4}|[0-9]{5}[DF])$"
     ///////////////////////////////////////////////////////////////////////////
     // 以下摘自http://tool.oschina.net/regex
     ///////////////////////////////////////////////////////////////////////////
@@ -67,45 +71,45 @@ object RegexConstants {
     /**
      * 正则：双字节字符(包括汉字在内)
      */
-    @JvmField val REGEX_DOUBLE_BYTE_CHAR = "[^\\x00-\\xff]"
+    const val REGEX_DOUBLE_BYTE_CHAR = "[^\\x00-\\xff]"
     /**
      * 正则：空白行
      */
-    @JvmField val REGEX_BLANK_LINE = "\\n\\s*\\r"
+    const val REGEX_BLANK_LINE = "\\n\\s*\\r"
     /**
      * 正则：QQ号
      */
-    @JvmField val REGEX_TENCENT_NUM = "[1-9][0-9]{4,}"
+    const val REGEX_TENCENT_NUM = "[1-9][0-9]{4,}"
     /**
      * 正则：中国邮政编码
      */
-    @JvmField val REGEX_ZIP_CODE = "[1-9]\\d{5}(?!\\d)"
+    const val REGEX_ZIP_CODE = "[1-9]\\d{5}(?!\\d)"
     /**
      * 正则：正整数
      */
-    @JvmField val REGEX_POSITIVE_INTEGER = "^[1-9]\\d*$"
+    const val REGEX_POSITIVE_INTEGER = "^[1-9]\\d*$"
     /**
      * 正则：负整数
      */
-    @JvmField val REGEX_NEGATIVE_INTEGER = "^-[1-9]\\d*$"
+    const val REGEX_NEGATIVE_INTEGER = "^-[1-9]\\d*$"
     /**
      * 正则：整数
      */
-    @JvmField val REGEX_INTEGER = "^-?[1-9]\\d*$"
+    const val REGEX_INTEGER = "^-?[1-9]\\d*$"
     /**
      * 正则：非负整数(正整数 + 0)
      */
-    @JvmField val REGEX_NOT_NEGATIVE_INTEGER = "^[1-9]\\d*|0$"
+    const val REGEX_NOT_NEGATIVE_INTEGER = "^[1-9]\\d*|0$"
     /**
      * 正则：非正整数（负整数 + 0）
      */
-    @JvmField val REGEX_NOT_POSITIVE_INTEGER = "^-[1-9]\\d*|0$"
+    const val REGEX_NOT_POSITIVE_INTEGER = "^-[1-9]\\d*|0$"
     /**
      * 正则：正浮点数
      */
-    @JvmField val REGEX_POSITIVE_FLOAT = "^[1-9]\\d*\\.\\d*|0\\.\\d*[1-9]\\d*$"
+    const val REGEX_POSITIVE_FLOAT = "^[1-9]\\d*\\.\\d*|0\\.\\d*[1-9]\\d*$"
     /**
      * 正则：负浮点数
      */
-    @JvmField val REGEX_NEGATIVE_FLOAT = "^-[1-9]\\d*\\.\\d*|-0\\.\\d*[1-9]\\d*$"
+    const val REGEX_NEGATIVE_FLOAT = "^-[1-9]\\d*\\.\\d*|-0\\.\\d*[1-9]\\d*$"
 }
