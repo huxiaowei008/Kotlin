f24b7b3957676c6f63b9a1b9e213c8696411689a
huxiaowei <huxiaowei008@126.com>
2018-07-16 20:00:54 +0800

    Merge branch 'master' of https://github.com/huxiaowei008/Kotlin
    # Conflicts:
    #	frame/src/main/java/com/hxw/frame/di/module/ClientModule.kt
    #	gradle/wrapper/gradle-wrapper.properties
    删了打算从新写

--- app/src/main/java/com/modoutech/kotlin/base/MyApplication.kt
+++ app/src/main/java/com/modoutech/kotlin/base/MyApplication.kt
@@ -1,45 +1,39 @@
 package com.modoutech.kotlin.base
 
-import android.content.Context
-import com.hxw.frame.base.delegate.AppDelegate
-import com.hxw.frame.base.delegate.AppLifecycle
-import com.modoutech.kotlin.di.DaggerAppComponent
-import dagger.android.AndroidInjector
-import dagger.android.support.DaggerApplication
-import kotlin.properties.Delegates
+import android.app.Application
 
 /**
  * @author hxw
  * @date 2017/9/20
  */
-class MyApplication : DaggerApplication() {
+class MyApplication : Application() {
 
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        return DaggerAppComponent
-                .builder()
-                .frameComponent(AppDelegate.FRAME_COMPONENT)
-                .build()
-
-    }
-
-    private var mAppDelegate: AppLifecycle by Delegates.notNull()
-
-    override fun attachBaseContext(base: Context) {
-        super.attachBaseContext(base)
-        mAppDelegate = AppDelegate(base)
-        mAppDelegate.attachBaseContext(base)
-    }
-
-    override fun onCreate() {
-        //AppDelegate.FRAME_COMPONENT在onCreate中初始化,applicationInjector在super.onCreate()中就会可能调用到
-        mAppDelegate.onCreate(this)
-        super.onCreate()
-
-    }
-
-    override fun onTerminate() {
-        mAppDelegate.onTerminate(this)
-        super.onTerminate()
-
-    }
+//    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+//        return DaggerAppComponent
+//                .builder()
+//                .frameComponent(AppDelegate.FRAME_COMPONENT)
+//                .build()
+//
+//    }
+//
+//    private var mAppDelegate: AppLifecycle by Delegates.notNull()
+//
+//    override fun attachBaseContext(base: Context) {
+//        super.attachBaseContext(base)
+//        mAppDelegate = AppDelegate(base)
+//        mAppDelegate.attachBaseContext(base)
+//    }
+//
+//    override fun onCreate() {
+//        //AppDelegate.FRAME_COMPONENT在onCreate中初始化,applicationInjector在super.onCreate()中就会可能调用到
+//        mAppDelegate.onCreate(this)
+//        super.onCreate()
+//
+//    }
+//
+//    override fun onTerminate() {
+//        mAppDelegate.onTerminate(this)
+//        super.onTerminate()
+//
+//    }
 }
--- app/src/main/java/com/modoutech/kotlin/mvp/login/LoginActivity.kt
+++ app/src/main/java/com/modoutech/kotlin/mvp/login/LoginActivity.kt
@@ -1,73 +1,63 @@
 package com.modoutech.kotlin.mvp.login
 
-import android.graphics.Color
-import android.os.Bundle
-import android.support.v7.content.res.AppCompatResources
-import com.hxw.frame.base.DaggerActivity
-import com.hxw.frame.imageloader.ImageLoader
-import com.hxw.frame.utils.*
-import com.jakewharton.rxbinding2.view.RxView
-import com.modoutech.kotlin.R
-import com.trello.rxlifecycle2.kotlin.bindToLifecycle
-import kotlinx.android.synthetic.main.activity_login.*
-import javax.inject.Inject
+import android.support.v7.app.AppCompatActivity
 
 /**
  * @author hxw
  * @date 2017/9/19
  */
-class LoginActivity : DaggerActivity(), LoginContract.View {
-
-    @Inject
-    lateinit var mPresenter: LoginContract.Presenter
-    @Inject
-    lateinit var imageLoader: ImageLoader
-
-    override fun getLayoutId(): Int = R.layout.activity_login
+class LoginActivity : AppCompatActivity(){
 
 
-    override fun init(savedInstanceState: Bundle?) {
-        cb_keep.buttonDrawable = AppCompatResources.getDrawable(this, R.drawable.check_box_selector)
-//        btn_login.setOnClickListener { mPresenter.login() }
-        imageLoader.displayRes(img_logo, R.mipmap.ic_launcher)
-
-        cb_keep.setOnCheckedChangeListener { buttonView, isChecked ->
-            setStatusBarDarkMode(isChecked)
-        }
-        RxView.clicks(btn_login)
-                .bindToLifecycle(this)//rxlifecycle的使用
-                .subscribe {
-                    mPresenter.login()
-
-                }
-        val map = "http://172.16.8.94:8080/GetNearByArea?token=195184551&lat=27.974277&lon=120.7336".urlRequestFormat()
-        tv_head.text = SpanUtils.newString("智能")
-                .setForegroundColor(Color.YELLOW)
-                .append("停车管理")
-                .setForegroundColor(Color.RED)
-                .setAbsoluteSize(10)
-                .append("系统")
-                .setBackgroundColor(Color.GRAY)
-                .create()
-    }
-
-    override fun showMessage(message: String) {
-        val t = et_username.text.toString()
-        UIUtils.toast("${t.isZh()}")
-
-    }
-
-    override fun launchActivity() {
-
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mPresenter.injectView(this)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mPresenter.dropView()
-    }
+//    lateinit var mPresenter: LoginContract.Presenter
+//
+//    lateinit var imageLoader: ImageLoader
+//
+//    override fun getLayoutId(): Int = R.layout.activity_login
+//
+//
+//    override fun init(savedInstanceState: Bundle?) {
+//        cb_keep.buttonDrawable = AppCompatResources.getDrawable(this, R.drawable.check_box_selector)
+////        btn_login.setOnClickListener { mPresenter.login() }
+//        imageLoader.displayRes(img_logo, R.mipmap.ic_launcher)
+//
+//        cb_keep.setOnCheckedChangeListener { buttonView, isChecked ->
+//            setStatusBarDarkMode(isChecked)
+//        }
+//        RxView.clicks(btn_login)
+//                .bindToLifecycle(this)//rxlifecycle的使用
+//                .subscribe {
+//                    mPresenter.login()
+//
+//                }
+//        val map = "http://172.16.8.94:8080/GetNearByArea?token=195184551&lat=27.974277&lon=120.7336".urlRequestFormat()
+//        tv_head.text = SpanUtils.newString("智能")
+//                .setForegroundColor(Color.YELLOW)
+//                .append("停车管理")
+//                .setForegroundColor(Color.RED)
+//                .setAbsoluteSize(10)
+//                .append("系统")
+//                .setBackgroundColor(Color.GRAY)
+//                .create()
+//    }
+//
+//    override fun showMessage(message: String) {
+//        val t = et_username.text.toString()
+//        UIUtils.toast("${t.isZh()}")
+//
+//    }
+//
+//    override fun launchActivity() {
+//
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        mPresenter.injectView(this)
+//    }
+//
+//    override fun onDestroy() {
+//        super.onDestroy()
+//        mPresenter.dropView()
+//    }
 }
